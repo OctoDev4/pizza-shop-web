c1f4475b261682210c600f85a649fce7a166cae3
Felipe Almeida <felipe.alhopato@gmail.com>
2024-08-13 23:03:36 -0300

    app-layout

--- src/_layouts/app.tsx
+++ src/_layouts/app.tsx
@@ -1,12 +1,16 @@
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { Header } from "@/components/header.tsx";
 
-export function AppLayout(){
-    return(
-        <div>
-            <h1>header</h1>
-            <div>
-                <Outlet/>
+export function AppLayout() {
+    return (
+        <div className="min-h-screen flex flex-col antialiased">
+            <Header />
+            <div className="flex-1 flex flex-col gap-4 p-8 pt-6">
+                <Outlet />
             </div>
+            <footer className="flex items-center justify-center p-4 bg-gray-800 text-white">
+                ooo
+            </footer>
         </div>
-    )
+    );
 }
