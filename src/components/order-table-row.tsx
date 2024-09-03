0c6f312c4d9101fcde1e7ef4f43f478ed03ab7ee
Felipe Almeida <felipe.alhopato@gmail.com>
2024-09-03 20:36:30 -0300

    date-ranger-picker

--- src/components/order-table-row.tsx
+++ src/components/order-table-row.tsx
@@ -1,21 +1,24 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { formatDistanceToNow } from 'date-fns'
-import { ptBR } from 'date-fns/locale'
-import { ArrowRight, Search, X } from 'lucide-react'
-import { useState } from 'react'
+// Importa hooks e utilitários do React Query e outras bibliotecas
+import {useMutation, useQueryClient} from '@tanstack/react-query'
+import {formatDistanceToNow} from 'date-fns'
+import {ptBR} from 'date-fns/locale'
+import {ArrowRight, Search, X} from 'lucide-react'
+import {useState} from 'react'
 
-import { approveOrder } from '@/api/approve-order'
-import { cancelOrder } from '@/api/cancel-order'
-import { deliverOrder } from '@/api/deliver-order'
-import { dispatchOrder } from '@/api/dispatch-order'
-import { GetOrdersResponse } from '@/api/get-orders'
-import { OrderStatus } from '@/components/order-status'
-import { Button } from '@/components/ui/button'
-import { Dialog, DialogTrigger } from '@/components/ui/dialog'
-import { TableCell, TableRow } from '@/components/ui/table'
+// Importa funções de API e componentes personalizados
+import {approveOrder} from '@/api/approve-order'
+import {cancelOrder} from '@/api/cancel-order'
+import {deliverOrder} from '@/api/deliver-order'
+import {dispatchOrder} from '@/api/dispatch-order'
+import {GetOrdersResponse} from '@/api/get-orders'
+import {OrderStatus} from '@/components/order-status'
+import {Button} from '@/components/ui/button'
+import {Dialog, DialogTrigger} from '@/components/ui/dialog'
+import {TableCell, TableRow} from '@/components/ui/table'
 
-import { OrderDetails } from './order-details'
+import {OrderDetails} from './order-details'
 
+// Define as propriedades esperadas para o componente OrderTableRow
 interface OrderTableRowProps {
     order: {
         orderId: string
@@ -26,25 +29,32 @@ interface OrderTableRowProps {
     }
 }
 
-export function OrderTableRow({ order }: OrderTableRowProps) {
+// Componente que representa uma linha da tabela de pedidos
+export function OrderTableRow({order}: OrderTableRowProps) {
+    // Estado local para controlar a abertura dos detalhes do pedido
     const [isDetailsOpen, setIsDetailsOpen] = useState(false)
+    // Instancia o cliente de queries do React Query
     const queryClient = useQueryClient()
 
+    // Função para atualizar o status de um pedido no cache do React Query
     function updateOrderStatusOnCache(orderId: string, status: OrderStatus) {
+        // Obtém os dados do cache relacionados a pedidos
         const ordersListCache = queryClient.getQueriesData<GetOrdersResponse>({
             queryKey: ['orders'],
         })
 
+        // Atualiza o status do pedido específico no cache
         ordersListCache.forEach(([cacheKey, cacheData]) => {
             if (!cacheData) {
                 return
             }
 
+            // Atualiza o cache com o novo status do pedido
             queryClient.setQueryData<GetOrdersResponse>(cacheKey, {
                 ...cacheData,
                 orders: cacheData.orders.map((order) => {
                     if (order.orderId === orderId) {
-                        return { ...order, status }
+                        return {...order, status}
                     }
 
                     return order
@@ -53,50 +63,66 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
         })
     }
 
-    const { mutateAsync: cancelOrderFn, isPending: isCancelingOrder } =
+    {/*
+
+        on sucess retorna onSuccess: async (data, variables, context) => {
+        }
+
+        data: Este parâmetro contém os dados retornados pela função de mutação (mutationFn) após a execução bem-sucedida.
+
+        variables: Estes são os mesmos parâmetros que foram passados para a função de mutação ao chamá-la. Por exemplo, no caso de approveOrderFn({ orderId: order.orderId }), variables conterá { orderId: order.orderId }.
+
+
+context: Este parâmetro é opcional e pode ser usado para armazenar e acessar informações entre onMutate (uma função chamada antes da mutação começar) e onSuccess.
+      */
+    }
+
+    // Mutations para diferentes ações de pedidos, cada uma com seu status e função de callback de sucesso
+    const {mutateAsync: cancelOrderFn, isPending: isCancelingOrder} =
         useMutation({
             mutationFn: cancelOrder,
-            async onSuccess(_, { orderId }) {
+            async onSuccess(_, {orderId}) {
                 updateOrderStatusOnCache(orderId, 'canceled')
             },
         })
 
-    const { mutateAsync: approveOrderFn, isPending: isApprovingOrder } =
+    const {mutateAsync: approveOrderFn, isPending: isApprovingOrder} =
         useMutation({
             mutationFn: approveOrder,
-            async onSuccess(_, { orderId }) {
+            async onSuccess(_, {orderId}) {
                 updateOrderStatusOnCache(orderId, 'processing')
             },
         })
 
-    const { mutateAsync: dispatchOrderFn, isPending: isDispatchingOrder } =
+    const {mutateAsync: dispatchOrderFn, isPending: isDispatchingOrder} =
         useMutation({
             mutationFn: dispatchOrder,
-            async onSuccess(_, { orderId }) {
+            async onSuccess(_, {orderId}) {
                 updateOrderStatusOnCache(orderId, 'delivering')
             },
         })
 
-    const { mutateAsync: deliverOrderFn, isPending: isDeliveringOrder } =
+    const {mutateAsync: deliverOrderFn, isPending: isDeliveringOrder} =
         useMutation({
             mutationFn: deliverOrder,
-            async onSuccess(_, { orderId }) {
+            async onSuccess(_, {orderId}) {
                 updateOrderStatusOnCache(orderId, 'delivered')
             },
         })
 
+    // Retorna a linha da tabela com as células de dados e ações
     return (
         <TableRow>
             <TableCell>
                 <Dialog open={isDetailsOpen} onOpenChange={setIsDetailsOpen}>
                     <DialogTrigger asChild>
                         <Button variant="outline" size="xs">
-                            <Search className="h-3 w-3" />
+                            <Search className="h-3 w-3"/>
                             <span className="sr-only">Detalhes do pedido</span>
                         </Button>
                     </DialogTrigger>
 
-                    <OrderDetails open={isDetailsOpen} orderId={order.orderId} />
+                    <OrderDetails open={isDetailsOpen} orderId={order.orderId}/>
                 </Dialog>
             </TableCell>
             <TableCell className="font-mono text-xs font-medium">
@@ -109,7 +135,7 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
                 })}
             </TableCell>
             <TableCell>
-                <OrderStatus status={order.status} />
+                <OrderStatus status={order.status}/>
             </TableCell>
             <TableCell className="font-medium">{order.customerName}</TableCell>
             <TableCell className="font-medium">
@@ -124,9 +150,9 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
                         variant="outline"
                         disabled={isApprovingOrder}
                         size="xs"
-                        onClick={() => approveOrderFn({ orderId: order.orderId })}
+                        onClick={() => approveOrderFn({orderId: order.orderId})}
                     >
-                        <ArrowRight className="mr-2 h-3 w-3" />
+                        <ArrowRight className="mr-2 h-3 w-3"/>
                         Aprovar
                     </Button>
                 )}
@@ -136,9 +162,9 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
                         variant="outline"
                         disabled={isDispatchingOrder}
                         size="xs"
-                        onClick={() => dispatchOrderFn({ orderId: order.orderId })}
+                        onClick={() => dispatchOrderFn({orderId: order.orderId})}
                     >
-                        <ArrowRight className="mr-2 h-3 w-3" />
+                        <ArrowRight className="mr-2 h-3 w-3"/>
                         Em entrega
                     </Button>
                 )}
@@ -148,9 +174,9 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
                         variant="outline"
                         disabled={isDeliveringOrder}
                         size="xs"
-                        onClick={() => deliverOrderFn({ orderId: order.orderId })}
+                        onClick={() => deliverOrderFn({orderId: order.orderId})}
                     >
-                        <ArrowRight className="mr-2 h-3 w-3" />
+                        <ArrowRight className="mr-2 h-3 w-3"/>
                         Entregue
                     </Button>
                 )}
@@ -161,11 +187,11 @@ export function OrderTableRow({ order }: OrderTableRowProps) {
                         !['pending', 'processing'].includes(order.status) ||
                         isCancelingOrder
                     }
-                    onClick={() => cancelOrderFn({ orderId: order.orderId })}
+                    onClick={() => cancelOrderFn({orderId: order.orderId})}
                     variant="ghost"
                     size="xs"
                 >
-                    <X className="mr-2 h-3 w-3" />
+                    <X className="mr-2 h-3 w-3"/>
                     Cancelar
                 </Button>
             </TableCell>
--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -1,12 +1,19 @@
+import { useQuery } from '@tanstack/react-query'
+import { subDays } from 'date-fns'
+import { useMemo, useState } from 'react'
+import { DateRange } from 'react-day-picker'
 import {
     CartesianGrid,
     Line,
     LineChart,
-    ResponsiveContainer, Tooltip,
+    ResponsiveContainer,
+    Tooltip,
     XAxis,
     YAxis,
 } from 'recharts'
+import colors from 'tailwindcss/colors'
 
+import { getDailyRevenueInPeriod } from '@/api/get-daily-revenue-in-period'
 import {
     Card,
     CardContent,
@@ -14,66 +21,92 @@ import {
     CardHeader,
     CardTitle,
 } from '@/components/ui/card'
-
-// Dados fictícios representando a receita diária em um período de 7 dias
-const data = [
-    { date: '10/12', revenue: 1200 },
-    { date: '11/12', revenue: 800 },
-    { date: '12/12', revenue: 900 },
-    { date: '13/12', revenue: 400 },
-    { date: '14/12', revenue: 2300 },
-    { date: '15/12', revenue: 800 },
-    { date: '16/12', revenue: 640 },
-]
+import { Label } from '@/components/ui/label'
+import { DateRangePicker } from "@/components/ui/date-ranger-picker.tsx";
 
 export function RevenueChart() {
+    // Estado para controlar o intervalo de datas selecionado
+    const [dateRange, setDateRange] = useState<DateRange | undefined>({
+        from: subDays(new Date(), 7), // Data de início: 7 dias atrás
+        to: new Date(), // Data de fim: hoje
+    });
+
+    // Hook useQuery para buscar os dados de receita diária no período selecionado
+    const { data: dailyRevenueInPeriod } = useQuery({
+        queryKey: ['metrics', 'daily-revenue-in-period', dateRange],
+        queryFn: () =>
+            getDailyRevenueInPeriod({
+                from: dateRange?.from,
+                to: dateRange?.to,
+            }),
+    });
+
+    // Transformar os dados recebidos para o formato necessário para o gráfico
+    const chartData = useMemo(() => {
+        return dailyRevenueInPeriod?.map((chartItem) => {
+            return {
+                date: chartItem.date,
+                receipt: chartItem.receipt / 100, // Converter centavos para reais
+            };
+        });
+    }, [dailyRevenueInPeriod]);
+
     return (
         <Card className="col-span-6">
+            {/* Cabeçalho do cartão */}
             <CardHeader className="flex-row items-center justify-between pb-8">
                 <div className="space-y-1">
-                    {/* Título do gráfico */}
                     <CardTitle className="text-base font-medium">
                         Receita no período
                     </CardTitle>
-                    {/* Descrição do gráfico */}
                     <CardDescription>Receita diária no período</CardDescription>
+                </div>
+
+                <div className="flex items-center gap-3">
+                    <Label>Período</Label>
+                    <DateRangePicker date={dateRange} onDateChange={setDateRange} />
                 </div>
             </CardHeader>
             <CardContent>
-                {/* Container que ajusta automaticamente o gráfico ao tamanho do contêiner pai */}
-                <ResponsiveContainer width="100%" height={240}>
-                    {/* Criação do gráfico de linha com os dados fornecidos */}
-                    <LineChart data={data} style={{ fontSize: 12 }}>
-                        {/* Eixo X representando as datas */}
-                        <XAxis dataKey="date" axisLine={false} tickLine={false} dy={16} />
-
-                        {/* Eixo Y representando a receita, formatada como moeda brasileira */}
-                        <YAxis
-                            dataKey='revenue'
-                            stroke="#888"
-                            axisLine={false}
-                            tickLine={false}
-                            width={80}
-                            tickFormatter={(value: number) =>
-                                value.toLocaleString('pt-BR', {
-                                    style: 'currency',
-                                    currency: 'BRL',
-                                })
-                            }
-                        />
-                        {/* Linha do gráfico que conecta os pontos de receita ao longo do período */}
-                        <Line
-                            stroke={'#EA580C'} // Cor da linha usando uma cor da paleta Tailwind
-                            type="natural"
-                            strokeWidth={2} // Espessura da linha
-                            dataKey="revenue" // Chave que especifica quais dados plotar na linha
-                        />
-                        {/* Tooltip que aparece ao passar o mouse sobre os pontos da linha */}
-                        <Tooltip/>
-                        <CartesianGrid vertical={false} className='stroke-muted'/>
-                    </LineChart>
-                </ResponsiveContainer>
+                {/* Renderiza o gráfico se houver dados disponíveis */}
+                {chartData && (
+                    <ResponsiveContainer width="100%" height={240}>
+                        <LineChart data={chartData} style={{ fontSize: 12 }}>
+                            {/* Eixo X do gráfico */}
+                            <XAxis
+                                dataKey="date"
+                                axisLine={false} // Ocultar linha do eixo
+                                tickLine={false} // Ocultar linhas dos ticks
+                                dy={16} // Ajuste de posição vertical dos ticks
+                            />
+                            {/* Eixo Y do gráfico */}
+                            <YAxis
+                                stroke="#888" // Cor da linha do eixo Y
+                                axisLine={false} // Ocultar linha do eixo
+                                tickLine={false} // Ocultar linhas dos ticks
+                                width={80} // Largura do eixo Y
+                                tickFormatter={(value: number) =>
+                                    value.toLocaleString('pt-BR', {
+                                        style: 'currency',
+                                        currency: 'BRL',
+                                    })
+                                }
+                            />
+                            {/* Tooltip para mostrar informações ao passar o mouse */}
+                            <Tooltip />
+                            {/* Grid do gráfico */}
+                            <CartesianGrid vertical={false} className="stroke-muted" />
+                            {/* Linha do gráfico */}
+                            <Line
+                                stroke={colors.violet[500]} // Cor da linha
+                                type="linear" // Tipo de linha
+                                strokeWidth={2} // Largura da linha
+                                dataKey="receipt" // Chave dos dados para a linha
+                            />
+                        </LineChart>
+                    </ResponsiveContainer>
+                )}
             </CardContent>
         </Card>
-    )
+    );
 }
